b3c6d383946118f88ce13362795d456ea12e547f
Sunil Band <sunilband0007@gmail.com>
2023-04-23 14:23:14 +0530

    tableData error

--- src/pages/components/Table.tsx
+++ src/pages/components/Table.tsx
@@ -2,29 +2,34 @@ import TableBody from "./TableBody";
 import TableHead from "./TableHead";
 import { useSortableTable } from "../../../logic/useSortableTable";
 
-
-import React from 'react'
+import React from "react";
 
 type Props = {
-  caption:any,
-  data:any, 
-  columns:any,
-  setRows:any,
-  rows:any,
-}
+  caption: any;
+  data: any;
+  columns: any;
+  setRows: any;
+  rows: any;
+};
 
 const Table = (props: Props) => {
-  const [tableData, handleSorting] = useSortableTable(props.data, props.columns);
+  const [tableData, handleSorting] = useSortableTable(
+    props.data?props.data:[],
+    props.columns?props.columns:[]
+  );
   return (
-          <><h2 className="text-4xl text-center mt-4 mb-4">{props.caption}</h2>
-            <table className="table">
-              <TableHead {...{ columns:props.columns, handleSorting }} setRows={props.setRows} rows={props.rows} />
-              <TableBody {...{ columns:props.columns, tableData }} />
-            </table>
-          </>
-  )
-}
+    <>
+      <h2 className="text-4xl text-center mt-4 mb-4">{props.caption}</h2>
+      <table className="table">
+        <TableHead
+          {...{ columns: props.columns, handleSorting }}
+          setRows={props.setRows}
+          rows={props.rows}
+        />
+        <TableBody {...{ columns: props.columns, tableData }} />
+      </table>
+    </>
+  );
+};
 
-export default Table
-
-
+export default Table;
